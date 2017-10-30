e12a24ebb74cb3c8244845dfb5dfe3fd1db99ca6
peter.osz <itharius@gmail.com>
2017-10-30 13:52:43 +0100

    created file structure and a simple RunningProcesses class

---
+++ ProcessNoteMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ProcessNote
+{
+    class ProcessNoteMain
+    {
+
+        public static void Main(string[] args)
+        {
+            RunningProcesses runningProcesses = new RunningProcesses();
+            runningProcesses.ShowRunningProcesses();
+        }
+    }
+}
---
+++ RunningProcesses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ProcessNote
+{
+    class RunningProcesses
+    {
+        public RunningProcesses()
+        {
+       
+        }
+
+        public void ShowRunningProcesses()
+        {
+            Process[] localAll = Process.GetProcesses();
+            foreach (Process local in localAll)
+            {
+                Console.WriteLine(local.ToString());
+            }
+        }
+}
+}
